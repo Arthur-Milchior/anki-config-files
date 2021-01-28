1b7092de07321fe5a5c5479d34d20c91af13b9ad
Arthur Milchior <Arthur@Milchior.fr>
2021-01-28 17:51:03 +0100

    NF: update addToGear

--- addToGear.py
+++ addToGear.py
@@ -1,10 +1,17 @@
-from anki.hooks import addHook
+from aqt.hooks_gen import deck_browser_will_show_options_menu
+
 
 
 def addActionToGear(fun, text):
-    """fun -- takes an argument, the did
+    """ Add an entry to deck gear, in deck browser, with text calling function.
+
+    fun -- takes an argument, the did
     text -- what's written in the gear."""
     def aux(m, did):
+        """Call the function added to the gear by addActionToGear.
+
+        m -- the menu entry
+        did -- the deck whose gear is clicked."""
         a = m.addAction(text)
         a.triggered.connect(lambda b, did=did: fun(did))
-    addHook("showDeckOptions", aux)
+    deck_browser_will_show_options_menu.append(aux)
